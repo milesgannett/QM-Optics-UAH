a1a62056f564de7e468b726afc215932fb87e5d4
Miles Gannett <milesagannett@gmail.com>
2026-06-10 12:32:32 -0500

    set up on lab laptop

---
+++ HVA200_control/main.py
@@ -0,0 +1,27 @@
+'''
+
+Author: Miles Gannett
+University of Alabama in Huntsville, Department of Physics and Astronomy
+June 2026
+
+Description: This script interfaces with the Thorlabs HVA200 High Voltage 
+Amplifier (HVA) via serial COM port using the InstrumentKit package. 
+Work in progress.
+
+'''
+
+
+import numpy as np
+import instruments as ik
+import time
+from datetime import datetime
+import os
+
+def main():
+
+    
+
+    return
+
+if __name__ == "__main__":
+    main()
